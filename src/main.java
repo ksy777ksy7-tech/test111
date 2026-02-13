17cdab8d1983be79145befc5d20cfc8c3f3000e6
ksenny <cnhfecbrf@mail.ru>
2026-02-13 12:05:15 +0300

    iditing commit

--- src/main.java
+++ src/main.java
@@ -1,17 +1,18 @@
 public class main {
     public static void main(String[] args) {
-        int start = 100; // начальный баланс
-        int add = 1349; // сумма пополнений
+        int start = 1; // начальный баланс
+        int add = 134; // сумма пополнений
         int summ = start + add; //сумма без бонусов на счету
-        int bonus = summ/100; // сумма бонусов
-        int total = start + add + bonus; // итоговый счет
+        int bonus = 0; // сумма бонусов при сумме < 1000
 
-       if (summ > 1000) {
-           bonus = summ/100;
-
-                    System.out.println(total);
-
-
-                }
-            }
+        if (summ > 1000) {
+            bonus = summ / 100; //если сумма > 1000, то бонусы начисляются
+            // Если summ < 1000, то бонусы не насляются
         }
+        int total = summ + bonus;
+
+        System.out.println(total);
+
+
+    }
+}
